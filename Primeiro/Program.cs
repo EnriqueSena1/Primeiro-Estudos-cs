b2b133880745a1ac807b28cb1c24ff8513b6d09d
EnriqueSena1 <enriquesenacontato07@gmail.com>
2025-03-25 07:22:22 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!"); 
+{
+    int idade;
+    double salario, altura;
+    char genero;
+    string nome;
+
+    idade = 32;
+    salario = 4000.9;
+    altura = 1.73;
+    genero = 'F';
+    nome = "Eduarda";
+
+    Console.WriteLine(idade);
+    Console.WriteLine(salario);
+    Console.WriteLine(altura);
+    Console.WriteLine(genero);
+    Console.WriteLine(nome);
+}
