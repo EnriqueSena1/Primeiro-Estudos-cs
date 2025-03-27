fc6932f541f756fe1710f5181d0d14eaa06ec14c
EnriqueSena1 <enriquesenacontato07@gmail.com>
2025-03-27 06:48:55 -0300

    Fix - realizado pulcas auterações e teste a Aula 211

--- Primeiro/Program.cs
+++ Primeiro/Program.cs
@@ -1,20 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!"); 
-{
-    int idade;
-    double salario, altura;
-    char genero;
-    string nome;
+﻿using System;
+using System.Globalization;
 
-    idade = 32;
-    salario = 4000.9;
-    altura = 1.73;
-    genero = 'F';
-    nome = "Eduarda";
+namespace Primeiro {
+    class Program {
+        static void Main(string[] args) {
 
-    Console.WriteLine(idade);
-    Console.WriteLine(salario);
-    Console.WriteLine(altura);
-    Console.WriteLine(genero);
-    Console.WriteLine(nome);
+            CultureInfo CI = CultureInfo.InvariantCulture; // Variavel CI criada para abreviação da função de importar o . ponto
+
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 32;
+            salario = 4000.9;
+            altura = 1.73;
+            genero = 'F';
+            nome = "Eduarda";
+
+            Console.WriteLine(idade);
+            Console.WriteLine(salario.ToString("F2", CI));
+            Console.WriteLine(altura.ToString("F2",CI));
+            Console.WriteLine(genero);
+            Console.WriteLine(nome);
+        }
+
+    }
 }
+
+
